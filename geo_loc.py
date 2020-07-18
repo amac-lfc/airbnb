0b6aed76638c10ec80f699dec3574c69214ac4bb
Samvel Gevorgyan <32220164+gevosa@users.noreply.github.com>
2020-07-17 21:09:42 -0400

    fixed the error

--- geo_loc.py
+++ geo_loc.py
@@ -27,8 +27,6 @@ def choose_csv(main_data = file_name):
     Returns:
         data: a Pandas DataFrame object
     '''
-
-
     if main_data != None:
         data = pd.read_csv(file_name,index_col=['id'])
     else:
@@ -55,38 +53,49 @@ def pois_amenities(data):
     bars = []
     cafes = []
     unis = []
+    errors_a = 0 
     data_temp = data 
     for x in tqdm(range(len(data_temp)),desc='Amenities'):
-        pois_temp = ox.pois_from_point(point=(data_temp.latitude.iloc[x],data_temp.longitude.iloc[x]), dist=1000, tags=tags)  #get POIs (tags) for a point with latitude and longitude in a range dist (in meters) 
-        # pois_temp is a DataFrame with the information about each POI 
-        if 'amenity' in pois_temp: 
-            '''
-            Adding the number of POIs to corresponding arrays
-            '''
-            if len(pois_temp[pois_temp['amenity']=='restaurant'].dropna(axis=1, how='any')) > 0:
-                restaurants.append(len(pois_temp[pois_temp['amenity']=='restaurant'].dropna(axis=1, how='any')))  
-            else: 
-                restaurants.append(0)
-            if len(pois_temp[pois_temp['amenity']=='bar'].dropna(axis=1, how='any')) > 0:
-                bars.append(len(pois_temp[pois_temp['amenity']=='bar'].dropna(axis=1, how='any'))) 
-            else: 
-                bars.append(0)
-            if len(pois_temp[pois_temp['amenity']=='cafe'].dropna(axis=1, how='any')) > 0:
-                cafes.append(len(pois_temp[pois_temp['amenity']=='cafe'].dropna(axis=1, how='any'))) 
-            else: 
-                cafes.append(0)   
-            if len(pois_temp[pois_temp['amenity']=='university'].dropna(axis=1, how='any')) > 0:
-                unis.append(len(pois_temp[pois_temp['amenity']=='university'].dropna(axis=1, how='any'))) 
+        try:
+            pois_temp = ox.pois_from_point(point=(data_temp.latitude.iloc[x],data_temp.longitude.iloc[x]), dist=1000, tags=tags)  #get POIs (tags) for a point with latitude and longitude in a range dist (in meters) 
+            # pois_temp is a DataFrame with the information about each POI 
+            if 'amenity' in pois_temp: 
+                '''
+                Adding the number of POIs to corresponding arrays
+                '''
+                if len(pois_temp[pois_temp['amenity']=='restaurant'].dropna(axis=1, how='any')) > 0:
+                    restaurants.append(len(pois_temp[pois_temp['amenity']=='restaurant'].dropna(axis=1, how='any')))  
+                else: 
+                    restaurants.append(0)
+                if len(pois_temp[pois_temp['amenity']=='bar'].dropna(axis=1, how='any')) > 0:
+                    bars.append(len(pois_temp[pois_temp['amenity']=='bar'].dropna(axis=1, how='any'))) 
+                else: 
+                    bars.append(0)
+                if len(pois_temp[pois_temp['amenity']=='cafe'].dropna(axis=1, how='any')) > 0:
+                    cafes.append(len(pois_temp[pois_temp['amenity']=='cafe'].dropna(axis=1, how='any'))) 
+                else: 
+                    cafes.append(0)   
+                if len(pois_temp[pois_temp['amenity']=='university'].dropna(axis=1, how='any')) > 0:
+                    unis.append(len(pois_temp[pois_temp['amenity']=='university'].dropna(axis=1, how='any'))) 
+                else:
+                    unis.append(0)      
             else:
-                 unis.append(0)      
-        else:
-            restaurants.append(0)  # if len(pois_temp[pois_temp['amenity']=='restaurant'].dropna(axis=1, how='any')) > 0 else restaurants.append(0) 
-            bars.append(0)
-            cafes.append(0)
-            unis.append(0)
+                restaurants.append(0)  # if len(pois_temp[pois_temp['amenity']=='restaurant'].dropna(axis=1, how='any')) > 0 else restaurants.append(0) 
+                bars.append(0)
+                cafes.append(0)
+                unis.append(0)
+        except Exception:
+            errors_a += 1 
+            print("\n \n Exception handled \n")
+            restaurants.append(np.nan)  # if len(pois_temp[pois_temp['amenity']=='restaurant'].dropna(axis=1, how='any')) > 0 else restaurants.append(0) 
+            bars.append(np.nan)
+            cafes.append(np.nan)
+            unis.append(np.nan)
+            continue
     '''
     Appending the gathered counts to a new dataframe (extending the initial one) 
     '''
+    print("Errors encountared: {}".format(errors_a))
     data_temp['restaurants'] = restaurants 
     data_temp['bars'] = bars 
     data_temp['cafes'] = cafes
@@ -107,58 +116,66 @@ def pois_subway(data):
     walk_time = [] #walking time to the closest CTA station in seconds 
     data_temp = data
     # for i in tqdm(range(len(data_temp))):
+    errors_s = 0
     for x in tqdm(range(len(data_temp)), desc = 'Subway'):
-        pois_temp = ox.pois_from_point(point=(data_temp.latitude.iloc[x],data_temp.longitude.iloc[x]), dist=1000, tags=tags)  
-        if 'subway' in pois_temp and len(pois_temp[pois_temp['subway'] == 'yes'].dropna(axis=1, how='any')) > 0: # if there is a column 'subway' in the temporary dataset with POIs for a listing x it means that there are stations within 1000m
-            train = pois_temp[pois_temp['subway'] == 'yes'].dropna(axis=1, how='any') # slicing out the information about the subways stations (name and location)
-            cta.append((len(train['name'].unique()))) # adding the number of unique CTA stations (.unique because several entrences to the same station are recorded)
-            coordinates = [[data_temp.iloc[x].longitude,data_temp.iloc[x].latitude]] # extracting the long and lat of a listing x 
-            '''
-            The following function uses regular expressions (import re) to format the long and lat 
-            retrieved by the ox.pois_from_point method. It drops all the symbols that are not a number (^0-9), not a dot (^.), not a space (^ ), and not a negative sign (^-) 
-            ''' 
-            for x in range (len(train)):
-                loc_str = str(train.geometry.iloc[x])
-                loc_num = re.sub("[^0-9,^.,^ ,-]", "", loc_str).lstrip().split(' ')
-                for i in range(len(loc_num)): 
-                    loc_num[i] = float(loc_num[i]) 
+        try:
+            pois_temp = ox.pois_from_point(point=(data_temp.latitude.iloc[x],data_temp.longitude.iloc[x]), dist=1000, tags=tags)  
+            if 'subway' in pois_temp and len(pois_temp[pois_temp['subway'] == 'yes'].dropna(axis=1, how='any')) > 0: # if there is a column 'subway' in the temporary dataset with POIs for a listing x it means that there are stations within 1000m
+                train = pois_temp[pois_temp['subway'] == 'yes'].dropna(axis=1, how='any') # slicing out the information about the subways stations (name and location)
+                cta.append((len(train['name'].unique()))) # adding the number of unique CTA stations (.unique because several entrences to the same station are recorded)
+                coordinates = [[data_temp.iloc[x].longitude,data_temp.iloc[x].latitude]] # extracting the long and lat of a listing x 
+                '''
+                The following function uses regular expressions (import re) to format the long and lat 
+                retrieved by the ox.pois_from_point method. It drops all the symbols that are not a number (^0-9), not a dot (^.), not a space (^ ), and not a negative sign (^-) 
+                ''' 
+                for x in range (len(train)):
+                    loc_str = str(train.geometry.iloc[x])
+                    loc_num = re.sub("[^0-9,^.,^ ,-]", "", loc_str).lstrip().split(' ')
+                    for i in range(len(loc_num)): 
+                        loc_num[i] = float(loc_num[i]) 
+                    coordinates.append(loc_num)
+                '''
+                Read more about the OpenRouteService in their docs: https://openrouteservice-py.readthedocs.io/en/latest/ 
+                locations - a dicitonary with the pairs of long and lat (includes both the starting point and destination(s))
+                destinations - a list of indecies of point to which the distance is going to be measured (basicly just excludes the starting point)
+                profile - method of moving (some of the alternatives are “driving-car” and  “cycling-regular”)
+                metrics - 'duration': time in secodnds to destination
+                also - 'distance': distance in meters  
+                
+                '''
+                matrix = client.distance_matrix(
+                    locations = coordinates,
+                    destinations = [_ for _ in range(1,len(coordinates))],
+                    profile = 'foot-walking',
+                    metrics = ['duration'],
+                    validate = False,
+                )
+                walk_time.append(round((matrix['durations'][0][matrix['durations'][0].index(min(matrix['durations'][0]))]/60),2))
+            else:
+                cta.append(np.nan) 
+                pois_temp = ox.pois_from_point(point=(data_temp.latitude.iloc[x],data_temp.longitude.iloc[x]), dist=5000, tags=tags)  
+                train = pois_temp[pois_temp['subway'] == 'yes'].dropna(axis=1, how='any')
+                coordinates = [[data_temp.iloc[x].longitude,data_temp.iloc[x].latitude]]
+                for x in range (len(train)):
+                    loc_str = str(train.geometry.iloc[x])
+                    loc_num = re.sub("[^0-9,^.,^ ,-]", "", loc_str).lstrip().split(' ')
+                    for i in range(len(loc_num)): 
+                        loc_num[i] = float(loc_num[i]) 
                 coordinates.append(loc_num)
-            '''
-            Read more about the OpenRouteService in their docs: https://openrouteservice-py.readthedocs.io/en/latest/ 
-            locations - a dicitonary with the pairs of long and lat (includes both the starting point and destination(s))
-            destinations - a list of indecies of point to which the distance is going to be measured (basicly just excludes the starting point)
-            profile - method of moving (some of the alternatives are “driving-car” and  “cycling-regular”)
-            metrics - 'duration': time in secodnds to destination
-            also - 'distance': distance in meters  
-            
-            '''
-            matrix = client.distance_matrix(
-                locations = coordinates,
-                destinations = [_ for _ in range(1,len(coordinates))],
-                profile = 'foot-walking',
-                metrics = ['duration'],
-                validate = False,
-            )
-            walk_time.append(round((matrix['durations'][0][matrix['durations'][0].index(min(matrix['durations'][0]))]/60),2))
-        else:
-            cta.append(np.nan) 
-            pois_temp = ox.pois_from_point(point=(data_temp.latitude.iloc[x],data_temp.longitude.iloc[x]), dist=5000, tags=tags)  
-            train = pois_temp[pois_temp['subway'] == 'yes'].dropna(axis=1, how='any')
-            coordinates = [[data_temp.iloc[x].longitude,data_temp.iloc[x].latitude]]
-            for x in range (len(train)):
-                loc_str = str(train.geometry.iloc[x])
-                loc_num = re.sub("[^0-9,^.,^ ,-]", "", loc_str).lstrip().split(' ')
-                for i in range(len(loc_num)): 
-                    loc_num[i] = float(loc_num[i]) 
-            coordinates.append(loc_num)
-            matrix = client.distance_matrix(
-                locations = coordinates,
-                destinations = [_ for _ in range(1,len(coordinates))],
-                profile = 'foot-walking',
-                metrics = ['distance', 'duration'],
-                validate = False,
-            )
-            walk_time.append(round((matrix['durations'][0][matrix['durations'][0].index(min(matrix['durations'][0]))]/60),2))
+                matrix = client.distance_matrix(
+                    locations = coordinates,
+                    destinations = [_ for _ in range(1,len(coordinates))],
+                    profile = 'foot-walking',
+                    metrics = ['distance', 'duration'],
+                    validate = False,
+                )
+                walk_time.append(round((matrix['durations'][0][matrix['durations'][0].index(min(matrix['durations'][0]))]/60),2))
+        except Exception:
+            errors_s += 1 
+            print("\n \n Exception handled \n")
+            cta.append(np.nan)
+            walk_time.append(np.nan)
+    print("Errors encountared: {}".format(errors_s))
     data_temp['cta'] = cta
     data_temp['time_to_cta_minutes'] = walk_time
     data_cta = data_temp
@@ -172,12 +189,12 @@ def save_csv(dataframe):
 
 if __name__ == "__main__":
     start = time.time()
-    dataset = choose_csv(main_data = 'listings_cleaned.csv')
+    dataset = choose_csv()
     data_amenities = pois_amenities(dataset)
     new_data = pois_subway(data_amenities)
     end = time.time()
     save_csv(new_data)
-    print(end - start)
+    print('Run time: {}'.format(end - start))
 
 
 
